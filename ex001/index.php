29c2e1072f4d9e37a9a4f65fd19266916bd6c4fa
Arturo Ceceña <arturocecena@hotmail.com>
2023-04-21 09:58:08 -0600

    Creación del Hola mundo y la inforamción del server.

---
+++ ex001/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Me livrando da maldiçäo</title>
+</head>
+<body>
+    <h1>
+        <?php 
+            echo "Hola mundo \u{1F30E}"; //Aquí sí se utiliza el punto y coma para cerrar el código.
+            //Ademas, con el código que empieza con \u{} plasmé un emoji, esto en la página de: https://emojipedia.org 
+
+        ?>
+    </h1>
+    <p>Vamos a tentar no livrar da maldição</p>
+</body>
+</html>
