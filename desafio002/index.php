590912d32caafeecfc6c753787fe5206933cbf41
Arturo Ceceña <arturocecena@hotmail.com>
2023-04-25 11:04:15 -0600

    Completando código y comentarios del desafio 002.

--- desafio002/index.php
+++ desafio002/index.php
@@ -14,9 +14,14 @@
         <h1>Trabalhando com números aleatórios</h1>
         <p>Gerando um número aleatório entre 0 y 100</p>
         <?php
-        $numero = rand(0, 100);
+        $numero = mt_rand(0, 100);
         echo "<p>O valor gerado foi <strong>$numero</strong></p>";
-        ?>
+        //Hay varios tipo de rand(), el primero es ese, el menos recomendable, el mt_rand() es el mejor y el random_int() es el más seguro pero el más lento, usalo cuando tengas que encriptrar.
+
+        
+        ?>  
+
+
         <form action="index.php" method="get">
             <label for="submit"></label>
             <input type="submit" value="🔄 Gerar outro">
